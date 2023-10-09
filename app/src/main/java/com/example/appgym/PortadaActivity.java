318372d2312752639c44aeb9129cd5bac7a91c3d
davidmnt <david.montejano@hotmail.com>
2023-10-09 14:14:27 +0200

    AppGym

--- app/src/main/java/com/example/appgym/PortadaActivity.java
+++ app/src/main/java/com/example/appgym/PortadaActivity.java
@@ -2,7 +2,10 @@ package com.example.appgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PortadaActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class PortadaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
+        Button botonComenzar = findViewById(R.id.comenzar);
+
+        botonComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                comenzar();
+            }
+        });
+    }
+
+    private void comenzar(){
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
     }
 }
