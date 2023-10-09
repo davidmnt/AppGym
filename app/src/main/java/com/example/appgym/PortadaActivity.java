8c90f862b56946642086a2e2661c64d7959d5b5b
davidmnt <david.montejano@hotmail.com>
2023-10-09 12:51:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appgym/PortadaActivity.java
@@ -0,0 +1,14 @@
+package com.example.appgym;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PortadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+    }
+}
